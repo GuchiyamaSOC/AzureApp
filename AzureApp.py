dc5f2a7971cca0b401a4717ed1c826ed4cfd8901
GuchiyamaSOC <118040170+GuchiyamaSOC@users.noreply.github.com>
2023-03-07 16:27:50 +0900

    Create AzureApp.py

---
+++ AzureApp.py
@@ -0,0 +1,6 @@
+Credential{ 
+  "AadClientId": "5b662e2b-d456-4c32-a88e-10a5ca9e8cc0",
+  "AadSecret": "78c158b4-cca0-426c-9c6f-1a9f668fe413",
+  "AadTenantDomain": "mss.onmicrosoft.com",
+  "AadTenantId": "cfc511a1-95de-4458-8c5b-a4f31088ad85"
+}
